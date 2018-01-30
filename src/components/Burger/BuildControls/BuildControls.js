2d71215f3f34a81be587e27661307504a83d4b28
acbrent25 <adamchampagne@gmail.com>
2018-01-30 10:37:55 -0500

    app functioning and order now button working

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,10 +1,34 @@
 import React from 'react';
-import './BuildControls.css';
+
+
+
+import BuildControl from './BuildControl/BuildControl';
+
+import classes from './BuildControls.css';
+
+const controls = [
+  {label: 'Salad', type: 'salad'},
+  {label: 'Bacon', type: 'bacon'},
+  {label: 'Cheese', type: 'cheese'},
+  {label: 'Meat', type: 'meat'},
+];
 
 const buildControls = (props) => {
   return (
-    <div>
-      
+    <div className={classes.BuildControls}>
+    <p>Current Price: <strong>{props.price.toFixed(2)}</strong></p>
+      {controls.map(ctrl => (
+        <BuildControl 
+        key={ctrl.label} 
+        label={ctrl.label}
+        added={() => props.ingedientAdded(ctrl.type)}
+        removed={() => props.ingedientRemoved(ctrl.type)}
+        disabled={props.disabled[ctrl.type]}
+        />
+      ))}
+      <button 
+      className={classes.OrderButton}
+      disabled={!props.purchaseable}>ORDER NOW</button>
     </div>
   );
 };
